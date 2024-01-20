dae8a90ea4c3e1d9d54b73287eec2c99377cbe82
Angelos <angelgrigoriu@gmail.com>
2024-01-20 22:36:14 +0000

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div>My app TEST 2</div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.jsx'),
+    background: path.resolve(__dirname, 'src/background/background.js'),
+    app: path.resolve(__dirname, 'src/components/App.jsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'Test App',
+      template: 'src/assets/template.html',
+    }),
+  ],
+};
