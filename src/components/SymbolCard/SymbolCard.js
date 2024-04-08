989d5a2b6e98e47bc518700beb363fb15e243764
Angelos <angelgrigoriu@gmail.com>
2024-04-08 22:43:57 +0100

    adding connection

--- src/components/SymbolCard/SymbolCard.js
+++ src/components/SymbolCard/SymbolCard.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import {
   Card,
   CardActions,
@@ -8,15 +8,44 @@ import {
 } from '@mui/material';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import { SymbolsContext } from '../../Contexts';
+import {
+  stringifyMessage,
+  constructSymbolString,
+  updateLocalStorageWatchlist,
+} from '../../helpers/helpers.mjs';
 
 const SymbolCard = ({ symbol, showPrice = false, setSearchTerm }) => {
-  const { watchlistSymbols, setWatchlistSymbols } = useContext(SymbolsContext);
+  const {
+    watchlistSymbols,
+    setWatchlistSymbols,
+    symbolsPriceDetails,
+    updateConnection,
+    binanceId = '',
+  } = useContext(SymbolsContext);
 
-  const handleAddSymbol = (e) => {
-    if (!watchlistSymbols) {
-      localStorage.setItem('Watchlist', JSON.stringify([symbol]));
-      setWatchlistSymbols([symbol]);
+  const [price, setPrice] = useState(
+    () =>
+      (symbolsPriceDetails?.s === symbol.symbol &&
+        parseFloat(symbolsPriceDetails.p)) ||
+      0,
+  );
+
+  useEffect(() => {
+    setPrice(
+      (prev) =>
+        (symbolsPriceDetails?.s === symbol.symbol &&
+          parseFloat(symbolsPriceDetails.p)) ||
+        prev,
+    );
+  }, [symbolsPriceDetails?.p]);
+
+  const handleAddSymbol = () => {
+    if (!watchlistSymbols.length) {
+      const updatedWatchlist = [symbol];
+      updateLocalStorageWatchlist(updatedWatchlist);
+      setWatchlistSymbols(updatedWatchlist);
     } else {
       const symbolExistsInWatchlist = watchlistSymbols.find(
         (symbolInWatchlist) => symbolInWatchlist.symbol === symbol.symbol,
@@ -24,13 +53,38 @@ const SymbolCard = ({ symbol, showPrice = false, setSearchTerm }) => {
 
       if (!symbolExistsInWatchlist) {
         const updatedWatchlist = [...watchlistSymbols, symbol];
-        localStorage.setItem('Watchlist', JSON.stringify(updatedWatchlist));
+        updateLocalStorageWatchlist(updatedWatchlist);
         setWatchlistSymbols(updatedWatchlist);
+
+        updateConnection(
+          stringifyMessage({
+            method: 'SUBSCRIBE',
+            params: [constructSymbolString(symbol)],
+            id: binanceId,
+          }),
+        );
       }
     }
 
     setSearchTerm('');
   };
+
+  const handleRemoveSymbol = () => {
+    const updatedWatchlist = watchlistSymbols.filter(
+      (listSymbol) => listSymbol.symbol !== symbol.symbol,
+    );
+    updateLocalStorageWatchlist(updatedWatchlist);
+    setWatchlistSymbols(updatedWatchlist);
+
+    updateConnection(
+      stringifyMessage({
+        method: 'UNSUBSCRIBE',
+        params: [constructSymbolString(symbol)],
+        id: binanceId,
+      }),
+    );
+  };
+
   return (
     <Card style={{ display: 'flex', flexDirection: 'row' }}>
       <CardContent>
@@ -41,11 +95,17 @@ const SymbolCard = ({ symbol, showPrice = false, setSearchTerm }) => {
         <Typography style={{ display: 'inline-block' }}>
           {symbol.symbol}
         </Typography>
+        {showPrice && <Typography>{price}</Typography>}
       </CardContent>
       <CardActions>
         {!showPrice && (
           <IconButton size='small' onClick={handleAddSymbol}>
             <AddIcon />
+          </IconButton>
+        )}
+        {showPrice && (
+          <IconButton size='small' onClick={handleRemoveSymbol}>
+            <RemoveIcon />
           </IconButton>
         )}
       </CardActions>
